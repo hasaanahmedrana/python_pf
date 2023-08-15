a98ced82a0b5af4203fc73a3ac2e4f88777f8b4c
hasaanahmedrana <132395812+hasaanahmedrana@users.noreply.github.com>
2023-08-16 00:07:17 +0500

    Add files via upload

---
+++ Lab 2 Task1.py
@@ -0,0 +1,14 @@
+x=int(input('enter number of seconds:'))
+a=x//3600
+b=a*3600
+c=(x-b)//60
+d=c*60
+e=x-(b+d)
+print('number of hours:',a)
+print('number of min:',c)
+print("number of seconds:",e)
+
+
+
+
+        
