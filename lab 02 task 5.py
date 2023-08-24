ba8b25bc86db8649821453c2ae85f764a8e8a45d
hasaanahmedrana <132395812+hasaanahmedrana@users.noreply.github.com>
2023-08-24 20:54:46 +0500

    Add files via upload

---
+++ lab 02 task 5.py
@@ -0,0 +1,7 @@
+x=input('Enter first string')
+y=input('Enter second string')
+print('Length of first string:',len(x))
+print('Length of second string:',len(y))
+a=len(x)
+b=len(y)
+print('Sum of lenght of both strings:',a+b)
