054652b6dd2a74dff6b10d4b4f31616adaedede1
hasaanahmedrana <132395812+hasaanahmedrana@users.noreply.github.com>
2023-10-02 17:28:12 +0500

    Add files via upload

---
+++ 16 task 1.py
@@ -0,0 +1,15 @@
+r=int(input('Row:'))
+rcontroller=r
+i=1
+while i<=rcontroller:
+    j=1
+    limit=r
+    n=1
+    while j <=limit:
+        print(n,end=' ')
+        j=j+1
+        limit=limit+1
+        n=n+1
+    n=n+1
+    i=i+1
+
